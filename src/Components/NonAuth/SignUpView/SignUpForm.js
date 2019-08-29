4c18df6ebdc578c07cab58da79fac3d134deb5dc
Pedro Alfonso <p.alfonsoag@gmail.com>
2019-08-28 22:47:17 -0400

    basic form

--- src/Components/NonAuth/SignUpView/SignUpForm.js
+++ src/Components/NonAuth/SignUpView/SignUpForm.js
@@ -1,17 +1,63 @@
 import React from "react";
-import { View, Text, StyleSheet, TextInput } from "react-native";
+import { View, Text, StyleSheet, TextInput, Button } from "react-native";
 import { Field, reduxForm } from "redux-form";
 
-const fieldName = () => {
-  return <TextInput placeholder={"Text from Field Name"}></TextInput>;
+const fieldComponent = ({
+  input,
+  meta,
+  placeholder,
+  keyboardType,
+  secureTextEntry,
+  autoCapitalize
+}) => {
+  return (
+    <TextInput
+      placeholder={placeholder}
+      onChangeText={input.onChange}
+      value={input.value}
+      keyboardType={keyboardType ? keyboardType : "default"}
+      secureTextEntry={secureTextEntry}
+      autoCapitalize={autoCapitalize && autoCapitalize}
+    ></TextInput>
+  );
 };
 
 const SignUpForm = props => {
-  console.log(props);
   return (
     <View>
-      <Field name={"name"} component={fieldName}></Field>
-      <Text>redux form</Text>
+      <Field
+        name={"name"}
+        component={fieldComponent}
+        placeholder={"Name"}
+      ></Field>
+      <Field
+        name={"email"}
+        component={fieldComponent}
+        placeholder={"email@example.com"}
+        keyboardType={"email-address"}
+        autoCapitalize={"none"}
+      ></Field>
+      <Field
+        name={"password"}
+        component={fieldComponent}
+        placeholder={"*****"}
+        secureTextEntry
+        autoCapitalize={"none"}
+      ></Field>
+      <Field
+        name={"confirmation"}
+        component={fieldComponent}
+        placeholder={"*****"}
+        secureTextEntry
+        autoCapitalize={"none"}
+      ></Field>
+
+      <Button
+        title={"Register"}
+        onPress={props.handleSubmit(values => {
+          console.log(values);
+        })}
+      ></Button>
     </View>
   );
 };
