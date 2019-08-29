c1f706c507ee41a57c1c3a853080b43037a82b9a
Pedro Alfonso <p.alfonsoag@gmail.com>
2019-08-28 23:15:09 -0400

    validate fuction implementation

--- src/Components/NonAuth/SignUpView/SignUpForm.js
+++ src/Components/NonAuth/SignUpView/SignUpForm.js
@@ -11,15 +11,69 @@ const fieldComponent = ({
   autoCapitalize
 }) => {
   return (
-    <TextInput
-      placeholder={placeholder}
-      onChangeText={input.onChange}
-      value={input.value}
-      keyboardType={keyboardType ? keyboardType : "default"}
-      secureTextEntry={secureTextEntry}
-      autoCapitalize={autoCapitalize && autoCapitalize}
-    ></TextInput>
+    <React.Fragment>
+      <TextInput
+        placeholder={placeholder}
+        onChangeText={input.onChange}
+        value={input.value}
+        keyboardType={keyboardType ? keyboardType : "default"}
+        secureTextEntry={secureTextEntry}
+        autoCapitalize={autoCapitalize && autoCapitalize}
+      ></TextInput>
+      <Text>{meta.touched && meta.error && meta.error}</Text>
+    </React.Fragment>
   );
+};
+
+const validate = values => {
+  const errors = {};
+  let patt = new RegExp(
+    '^(([^<>()\\[\\]\\.,;:\\s@"]+(\\.[^<>()\\[\\]\\.,;:\\s@"]+)*)|(".+"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$'
+  );
+
+  if (!values.name) {
+    errors.name = "Required";
+  }
+  if (values.name && (values.name.length < 5 || values.name.length > 10)) {
+    errors.name = "Name length must be between 5 and 10 characters";
+  }
+  if (!values.email) {
+    errors.email = "Required";
+  } else {
+    let result = patt.test(values.email);
+    if (!result) {
+      errors.email = "Must be an valid email address";
+    }
+  }
+  if (!values.password) {
+    errors.password = "Required";
+  }
+  if (
+    values.password &&
+    (values.password.length < 5 || values.password.length > 15)
+  ) {
+    errors.password = "Password length must be between 5 and 15 characters";
+  }
+  if (!values.confirmation) {
+    errors.confirmation = "Required";
+  }
+  if (
+    values.confirmation &&
+    (values.confirmation.length < 5 || values.confirmation.length > 15)
+  ) {
+    errors.confirmation =
+      "Password Confirmation length must be between 5 and 15 characters";
+  }
+
+  if (
+    values.password &&
+    values.confirmation &&
+    values.password !== values.confirmation
+  ) {
+    errors.confirmation = "Your passwords must match";
+  }
+
+  return errors;
 };
 
 const SignUpForm = props => {
@@ -40,14 +94,14 @@ const SignUpForm = props => {
       <Field
         name={"password"}
         component={fieldComponent}
-        placeholder={"*****"}
+        placeholder={"Password"}
         secureTextEntry
         autoCapitalize={"none"}
       ></Field>
       <Field
         name={"confirmation"}
         component={fieldComponent}
-        placeholder={"*****"}
+        placeholder={"Password Confirmation"}
         secureTextEntry
         autoCapitalize={"none"}
       ></Field>
@@ -62,4 +116,4 @@ const SignUpForm = props => {
   );
 };
 
-export default reduxForm({ form: "SignUpForm" })(SignUpForm);
+export default reduxForm({ form: "SignUpForm", validate })(SignUpForm);
